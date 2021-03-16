6b2e229e2ac6f370e3296f69051b78404eebb4f9
cnmao <1098055977@qq.com>
2021-03-16 23:36:30 +0800

    第一次提交

---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")
+    val tempr = 10 ;
+
+    //A string template inserts a variable or expression into a string,
+    // and $ specifies that part of the string will be a variable or expression.
+    // Curly braces {} frame the expression, if any.
+    println("The water temperature is ${if (tempr > 50) "too hard" else "ok"}")
+}
