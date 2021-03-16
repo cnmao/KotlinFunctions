eded4639fff101ec7ffd5e93921d305e906b5be1
cnmao <1098055977@qq.com>
2021-03-16 23:43:18 +0800

    为什么提交不上去

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -5,5 +5,6 @@ fun main(args: Array<String>) {
     //A string template inserts a variable or expression into a string,
     // and $ specifies that part of the string will be a variable or expression.
     // Curly braces {} frame the expression, if any.
+
     println("The water temperature is ${if (tempr > 50) "too hard" else "ok"}")
 }
